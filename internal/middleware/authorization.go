261bdc6ef36d083103ca234871e0f5b2823f785f
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-08 16:11:55 +0530

    add authorization

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -8,4 +8,31 @@ import {
 	log "github.com/sirupsen/logrus"
 }
 
-var unAuthorized = errors.New("Invalid username or token");
+var UnAuthorizedError = errors.New("Invalid username or token") //custom unauthorized error 
+
+func Authorization(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error 
+		if username == "" || token == ""{
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w,UnAuthorizedError)
+			return
+		}
+		var database *tools.DatabaseInterface
+		databse, err = tools.NewDatabase()
+		if err != nil {
+			api.InternalErrorHandler(w)
+			return
+		}
+		var loginDetails *tools.LoginDetails 
+		loginDetails = (*database).GetUserLoginDetails(username)
+		if (loginDetails == nil || (token != (*&loginDetails).AuthToken)){
+			log.Error(unAuthorizedError)
+			api.RequestErrorHandler(w,UnAuthorizedError)
+			return
+		}
+		next.ServeHTTP(w,r) //this function in the line or in handler function for the endpoint if there's no middlerware left
+	})
+}
