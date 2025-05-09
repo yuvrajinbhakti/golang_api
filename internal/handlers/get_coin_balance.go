aa2974fdf9df1dc5128afb7be1a9c23dc88832e4
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-09 22:12:00 +0530

    Completing api

---
+++ internal/handlers/get_coin_balance.go
@@ -0,0 +1,48 @@
+package handlers
+import{
+	"net/http"
+	"encoding/json"
+	"github.com/yuvrajinbhak/golang-api/api"
+	log "github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
+}
+
+func GetCoinBalance(w http.ResponseWriter, r *http.Request){
+	var params = api.CoinBalanceParams{}
+	var decoder *schema.Decoder = schema.NewDecoder()
+	var err error 
+	err = decoder.Decode(&params,r.URL.Query())
+	if err != nil{
+		log.Error("Error decoding request parameters: ", err)
+		api.InternalErrorHandler(w)
+		return 
+	}
+
+	var database *tools.Database 
+	database,err = tools.NewDatabase()
+	if err !=nil{
+		api.InternalErrorHandler(w)
+		return 
+	}
+	
+	var tokenDetails *tools.CoinDetails
+	tokenDetails = (*database).GetUserCoins(params.Username)
+	if tokenDetails == nil{
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return 
+	}
+
+	var response = api.CoinBalanceResponse { 
+		Balance:  (*tokenDetails).Coins,
+		Code: http.StatusOk,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response) 
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return 
+	}
+}
